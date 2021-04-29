2a931e3d9a2964afe54a4d45d4a51684af6a60eb
Tony Kharioki <antolisah@gmail.com>
2021-04-29 23:28:01 +0300

    edit posts

---
+++ client/src/components/PostEdit.js
@@ -0,0 +1,16 @@
+import { Edit, SimpleForm, TextInput, DateInput } from 'react-admin';
+
+const PostEdit = (props) => {
+    return (
+        <Edit title='Edit post' {...props}>
+            <SimpleForm>
+                <TextInput disabled source='id' />
+                <TextInput source='title' />
+                <TextInput multiline source='body' />
+                <DateInput label='Puublished' source='publishedAt' />
+            </SimpleForm>
+        </Edit>
+    );
+}
+ 
+export default PostEdit;
